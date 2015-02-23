39c51a6a562dc87b108cbb1177c54f9721c40a9e
tksarah <tsarah8139@gmail.com>
2015-02-23 17:30:24 +0900

    Rename each myscript/*

--- cleansing.sh
+++ cleansing.sh
@@ -7,35 +7,35 @@ DIR=$(dirname $0)/myscript
 ####### SystemInfo #######
 echo 
 echo "SystemInfo Start"
-$DIR/systeminfo_current.pl $LOG 
+$DIR/systeminfo.pl $LOG 
 echo "SystemInfo Finish"
 sleep 3
 
 ####### Stats #######
 echo 
 echo "Stats Start"
-$DIR/all_current.pl $LOG $ITRE
+$DIR/all.pl $LOG $ITRE
 echo "Stats Finish"
 sleep 3
 
 ####### Latency #######
 echo
 echo "Latency Start"
-$DIR/latency_parse_beta.pl $LOG $ITRE
+$DIR/latency.pl $LOG $ITRE
 echo "Latency Finish"
 sleep 3
 
 ####### Throughput #######
 echo
 echo "Throughput Start"
-$DIR/throughput_parse_beta.pl $LOG $ITRE
+$DIR/throughput.pl $LOG $ITRE
 echo "Throughput Finish"
 sleep 3
 
 ####### Disk List #######
 echo
 echo "Disk List Start"
-$DIR/disk_parse_beta.pl $LOG $ITRE
+$DIR/disk.pl $LOG $ITRE
 echo "Disk List Finish"
 sleep 3
 
@@ -44,7 +44,7 @@ echo
 for i in nfsv3 cifs fcp iscsi
 do
 	echo -n "Workload $i Start ..." | tee -a ./workload.log
-	$DIR/workload_parse_my_current.pl $LOG $i $ITRE >> ./workload.log
+	$DIR/workload.pl $LOG $i $ITRE >> ./workload.log
 	echo " Finish" | tee -a ./workload.log
 done
 sleep 3
@@ -52,14 +52,14 @@ sleep 3
 ####### CIFS List #######
 echo
 echo "CIFS Stat Start"
-$DIR/cifs_stat_beta.pl $LOG $ITRE
+$DIR/cifs_stat.pl $LOG $ITRE
 echo "CIFS Stat Finish"
 sleep 3
 
 ####### LU List #######
 echo
 echo "LU List Start"
-$DIR/lun_parse_beta.pl $LOG $ITRE
+$DIR/lun.pl $LOG $ITRE
 echo "LU List Finish"
 
 ####### Full Workload #######
